93a4519a64bb1096d2f44e6c0de433a161f8599e
cnsecer <515343908@qq.com>
2015-07-13 06:54:09 +0800

    init application

---
+++ Application/Runtime/Cache/Home/68bce0ec7afdf8798ef65138f610dc8c.php
@@ -0,0 +1,12 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo ($title); ?></title>
+	<link rel="stylesheet" href="/Application/Home/View//Public/static/css/style.css">
+	<script type="text/javascript" src="/Application/Home/View//Public/static/js/init.js"></script>
+</head>
+<body>
+	<h3>单页</h3>
+</body>
+</html>
